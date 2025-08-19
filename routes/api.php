1bb39276908141549266688968ba93927eb67a3b
desoff2026 <luismiguel200365@gmail.com>
2025-08-19 00:17:06 -0500

    integracion scope de role

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('roles', RoleController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
